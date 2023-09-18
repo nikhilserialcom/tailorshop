ef30486dcd4bddf09f4cf473d551ea9cb2de20e8
nikhilserialcom <1nikhil.serialcominfotech@gmail.com>
2023-09-18 11:53:24 +0530

    add userid

--- Measurement/showmeasurement.php
+++ Measurement/showmeasurement.php
@@ -3,18 +3,27 @@
 require "../connection.php";
 header("content-type:application/json");
 
-$pant = "SELECT * FROM measurement ORDER BY id DESC LIMIT 1";
-$result = mysqli_query($con,$pant);
+if(isset($_POST['userId']))
+{
+    $userId = $_POST['userId'];
+    $pant = "SELECT * FROM measurement WHERE user_id = '$userId' ORDER BY id DESC LIMIT 1";
+    $result = mysqli_query($con,$pant);
 
-if (mysqli_num_rows($result)>0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $response['measurement'][] = $row;
+    if (mysqli_num_rows($result)>0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $response['measurement'][] = $row;
+        }
+    }
+    else
+    {
+        $response['error'] = "400";
+        $response['message'] = "user data not found";
     }
 }
 else
 {
-    $response['error'] = "400";
-    $response['message'] = "user data not found";
+    $response['failed'] = "500";
+    $response['message'] = "ERROR:";
 }
 
 echo json_encode($response);
--- rowcounter.php
+++ rowcounter.php
@@ -4,66 +4,75 @@ require 'connection.php';
 header("content-type:application/json");
 
 
-
-$successcount = "SELECT count(*) as success FROM measurement WHERE status = 'complete'";
-$successcount_query = mysqli_query($con,$successcount);
-
-if(mysqli_num_rows($successcount_query) > 0)
+if(isset($_POST['userId']))
 {
-    $row = mysqli_fetch_assoc($successcount_query);
-    $response['success'] = "200";
-    $response['complete'] = $row['success'];
+    $userId = $_POST['userId'];
+
+    $successcount = "SELECT count(*) as success FROM measurement WHERE user_id = '$userId' AND status = 'complete'";
+    $successcount_query = mysqli_query($con,$successcount);
+
+    if(mysqli_num_rows($successcount_query) > 0)
+    {
+        $row = mysqli_fetch_assoc($successcount_query);
+        $response['success'] = "200";
+        $response['complete'] = $row['success'];
+    }
+    else
+    {
+        $response['failed'] = "400";
+        $response['message'] = "record not found";
+    }
+
+
+    $pendingcount = "SELECT count(*) as pending FROM measurement WHERE user_id = '$userId' AND status = 'pending'";
+    $pendingcount_query = mysqli_query($con,$pendingcount);
+
+    if(mysqli_num_rows($pendingcount_query) > 0)
+    {
+        $row = mysqli_fetch_assoc($pendingcount_query);
+        $response['success'] = "200";
+        $response['pending'] = $row['pending'];
+    }
+    else
+    {
+        $response['failed'] = "400";
+        $response['message'] = "record not found";
+    }
+
+    $runningcount = "SELECT count(*) as running FROM measurement WHERE user_id = '$userId' AND status = 'running'";
+    $runningcount_query = mysqli_query($con,$runningcount);
+
+    if(mysqli_num_rows($runningcount_query) > 0)
+    {
+        $row = mysqli_fetch_assoc($runningcount_query);
+        $response['success'] = "200";
+        $response['running'] = $row['running'];
+    }
+    else
+    {
+        $response['failed'] = "400";
+        $response['message'] = "record not found";
+    }
+
+    $failcount = "SELECT count(*) as fail FROM measurement WHERE user_id = '$userId' AND status = 'fail'";
+    $failcount_query = mysqli_query($con,$failcount);
+
+    if(mysqli_num_rows($failcount_query) > 0)
+    {
+        $row = mysqli_fetch_assoc($failcount_query);
+        $response['success'] = "200";
+        $response['fail'] = $row['fail'];
+    }
+    else
+    {
+        $response['failed'] = "400";
+        $response['message'] = "record not found";
+    }
 }
 else
 {
-    $response['failed'] = "400";
-    $response['message'] = "record not found";
-}
-
-
-$pendingcount = "SELECT count(*) as pending FROM measurement WHERE status = 'pending'";
-$pendingcount_query = mysqli_query($con,$pendingcount);
-
-if(mysqli_num_rows($pendingcount_query) > 0)
-{
-    $row = mysqli_fetch_assoc($pendingcount_query);
-    $response['success'] = "200";
-    $response['pending'] = $row['pending'];
-}
-else
-{
-    $response['failed'] = "400";
-    $response['message'] = "record not found";
-}
-
-$runningcount = "SELECT count(*) as running FROM measurement WHERE status = 'running'";
-$runningcount_query = mysqli_query($con,$runningcount);
-
-if(mysqli_num_rows($runningcount_query) > 0)
-{
-    $row = mysqli_fetch_assoc($runningcount_query);
-    $response['success'] = "200";
-    $response['running'] = $row['running'];
-}
-else
-{
-    $response['failed'] = "400";
-    $response['message'] = "record not found";
-}
-
-$failcount = "SELECT count(*) as fail FROM measurement WHERE status = 'fail'";
-$failcount_query = mysqli_query($con,$failcount);
-
-if(mysqli_num_rows($failcount_query) > 0)
-{
-    $row = mysqli_fetch_assoc($failcount_query);
-    $response['success'] = "200";
-    $response['fail'] = $row['fail'];
-}
-else
-{
-    $response['failed'] = "400";
-    $response['message'] = "record not found";
+    $response['failed'] = "500";
+    $response['message'] = "ERROR:";
 }
 echo json_encode($response);
 ?>
